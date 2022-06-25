e199c575d8666038da954368f16cfab47f8b88b0
achaparro <achaparro@binwus.com>
2022-06-25 11:47:53 -0400

    feat: agregado de .env para manejo de variables de entorno y no exponer credenciales importantes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Colllage
  Repositorio web de imagenes realizado en flas + react
+
+ # Proceso para funcionamiento de la API
+ 1. crear entorno virtual (python3 -m venv venv)
+ 2. activar entorno virtual (. venv/bin/activate)
+ 3. instalar dependencias de requriements.txt (pip install -r requirements.txt)
+ 5. crear archivo .env con los datos DATABASE_URL y SECRET
+ 4. correr server.py (python server.py)
--- config.py
+++ config.py
@@ -5,11 +5,15 @@
     El micro framework de flask es capaz de leer este.
 """
 import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno
+load_dotenv()
 
 class Config(object):
     # SECRET_KEY = os.urandom(24)
-    SECRET_KEY = 'un_valor_random'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://biqpnxyiyjiaht:42e3ecd725496c9e9e8d83175402b1b937b0ed40218eec60cc95fdbe842e90e1@ec2-34-198-31-223.compute-1.amazonaws.com:5432/d5q4v32k2hp3je'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Configuracion de produccion 
